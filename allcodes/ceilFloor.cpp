c52217dc3dbfd47a90fdf7ec3c8399989f027be4
Bhavya Verma <me.bhavyaverma111@gmail.com>
2023-07-07 16:17:20 +0530

    updates

---
+++ allcodes/ceilFloor.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+     float v;
+      v= 10.0/3.0;
+    float ceils= ceil(v);
+      float floors= ceil(v);
+    cout<<ceils<<"  "floors;
+
+    return 0;
+}
